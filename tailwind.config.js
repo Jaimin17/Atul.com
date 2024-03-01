b08c1b9f86e9e0ca3bf040aa7002d20fab438f46
Krish Bhatt <krishbhatt1217@gmail.com>
2024-03-02 00:53:22 +0530

    Completed for desktop exprience!

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': "1932px",
+      '4xl': "2180px"
+    }
   },
   plugins: [],
 }
